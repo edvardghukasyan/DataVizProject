73203ee7bca38256a7a826c59effc00c6d4fadc2
Edvard <79043355+edvardghukasyan@users.noreply.github.com>
2024-05-29 09:52:15 +0400

    Update grades.py

--- pages/grades.py
+++ pages/grades.py
@@ -5,12 +5,12 @@ import os
 
 
 def app():
-    st.title("Grades Distribution")
+    # st.title("Grades Distribution")
 
-    script_path = os.path.dirname(__file__)
-    file_path = os.path.join(script_path, '../student_data 2.csv')
+    # script_path = os.path.dirname(__file__)
+    # file_path = os.path.join(script_path, '../student_data 2.csv')
 
-    df = pd.read_csv(file_path)
+    df = pd.read_csv('student_data.csv')
 
     fig = px.histogram(df, x='G3', nbins=20, title="Final Grade Distribution (G3)")
     st.plotly_chart(fig)
