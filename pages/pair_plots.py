f4a893c0c41f36b57858a5ac4478e0dba76f58d4
Edvard <79043355+edvardghukasyan@users.noreply.github.com>
2024-05-29 10:28:20 +0400

    Update pair_plots.py

--- pages/pair_plots.py
+++ pages/pair_plots.py
@@ -8,6 +8,15 @@ def app():
     # script_path = os.path.dirname(__file__)
     # file_path = os.path.join(script_path, '../student_data 2.csv')
 
-    df = pd.read_csv('student_data.csv')
-    fig = sns.pairplot(df, hue='G3')
-    st.pyplot(fig)
+    # df = pd.read_csv('student_data.csv')
+    # fig = sns.pairplot(df, hue='G3')
+    # st.pyplot(fig)
+
+    subset_columns = ['G1', 'G2', 'G3', 'studytime', 'failures', 'absences']  # Adjust the column names as needed
+    subset_df = df[subset_columns]
+    
+    # Generate the pair plot for the selected subset of columns
+    pair_plot = sns.pairplot(subset_df)
+    
+    # Display the pair plot
+    st.pyplot(pair_plot.fig)
