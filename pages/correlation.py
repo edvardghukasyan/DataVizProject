494aec2e79a4171292c650fcbafd31bbce1fd72e
Edvard <79043355+edvardghukasyan@users.noreply.github.com>
2024-05-29 09:51:44 +0400

    Update correlation.py

--- pages/correlation.py
+++ pages/correlation.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 import os
 
 def app():
-    st.title("Correlation Analysis")
-    script_path = os.path.dirname(__file__)
-    file_path = os.path.join(script_path, '../student_data 2.csv')
+    # st.title("Correlation Analysis")
+    # script_path = os.path.dirname(__file__)
+    # file_path = os.path.join(script_path, '../student_data 2.csv')
 
-    df = pd.read_csv(file_path)
+    df = pd.read_csv('student_data.csv')
     corr = df.corr()
     fig, ax = plt.subplots()
     sns.heatmap(corr, annot=True, cmap='coolwarm', ax=ax)
